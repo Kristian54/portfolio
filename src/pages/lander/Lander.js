77eba12d9e30d6bcbb840acd422d25a8a064a0ea
Kristian <garderkristian@gmail.com>
2024-09-01 17:21:40 +0200

    Optimized lander for small viewports

--- src/pages/lander/Lander.js
+++ src/pages/lander/Lander.js
@@ -3,12 +3,12 @@ import {BsGithub, BsInstagram, BsLinkedin, BsWhatsapp} from "react-icons/bs";
 
 export default function Lander() {
     return (
-        <div className="flex flex-col bg-gradient-to-br from-cyan-500 to-green-500 w-screen h-screen p-4 pr-24">
+        <div className="flex flex-col bg-gradient-to-br from-cyan-500 to-green-500 w-screen h-screen p-4 sm:pr-24">
 
             <div className="flex flex-row w-full h-full">
                 <div
-                    className="flex flex-col justify-center items-center animate-fly-in-from-bottom flex-1 bg-white/30 border border-white/40 rounded-lg drop-shadow-lg backdrop-blur-sm mb-2">
-                    <h1 className={'text-9xl font-light text-white'}>Kristian Nærum Garder</h1>
+                    className="flex flex-col justify-center items-center text-center animate-fly-in-from-bottom flex-1 bg-white/30 border border-white/40 rounded-lg drop-shadow-lg backdrop-blur-sm mb-2">
+                    <h1 className={'text-7xl sm:text-9xl font-light text-white'}>Kristian Nærum Garder</h1>
                     <div className={'flex flex-row absolute bottom-4 space-x-4'}>
                         <MediaIcon icon={<BsGithub color={'white'} size={40}/>} href={'https://github.com/Kristian54'}/>
                         <MediaIcon icon={<BsLinkedin color={'white'} size={40}/>} />
@@ -18,13 +18,13 @@ export default function Lander() {
                 </div>
             </div>
 
-            <div className="flex flex-row w-full h-full">
+            <div className="flex flex-col sm:flex-row w-full h-full">
                 <NavLink to={'/projects'}
-                    className="cursor-pointer animate-fly-in-from-top flex-1 bg-white/30 border border-white/40 rounded-lg drop-shadow-lg backdrop-blur-sm mr-2 mt-2 p-4">
+                    className="cursor-pointer animate-fly-in-from-top flex-1 bg-white/30 border border-white/40 rounded-lg drop-shadow-lg backdrop-blur-sm mb-2 sm:mb-0 sm:mr-2 mt-2 p-4">
                     <h1 className='text-2xl text-white font-light'>Projects</h1>
                 </NavLink>
                 <NavLink to={'/education'}
-                    className="cursor-pointer animate-fly-in-from-top flex-1 bg-white/30 border border-white/40 rounded-lg drop-shadow-lg backdrop-blur-sm ml-2 mt-2 p-4">
+                    className="cursor-pointer animate-fly-in-from-top flex-1 bg-white/30 border border-white/40 rounded-lg drop-shadow-lg backdrop-blur-sm sm:ml-2 mt-2 p-4">
                     <h1 className='text-2xl text-white font-light'>Education</h1>
                 </NavLink>
             </div>
